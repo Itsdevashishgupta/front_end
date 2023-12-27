2c9a7c3caeaf6e5001dc05ddfbba5d6f895358b7
devashish gupta <devashishgupta20102000@gmail.com>
2023-12-27 15:22:49 +0530

    add

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { Route, Routes,Router } from 'react-router-dom'
+import './App.css'
+import Sidebar, { SidebarItem } from './Home/Sidebar'
+// import { Files, FilesIcon, FolderOpenDot, IndianRupee, KanbanSquare, LayoutDashboard, Library, MessageCircleDashed, Shield, TimerIcon, Workflow, } from 'lucide-react'
+import Home from './Home/home'
+import Files from './file_manger/Files'
+import Quotation from './Quotation/Quotation'
+import Demo from './file_manger/demo'
+import Fetch from './file_manger/Fetchdata'
+function App() {
+
+  return (
+    <>
+    
+    <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/file/*' element={<Files/>}/>
+    <Route path='/quotation' element={<Quotation/>}/>
+    <Route path='/demo' element={<Demo/>}/>
+    <Route path="/item/:itemId" element={<Fetch />} />
+    <Route path="/item/:itemId" element={<Fetch />} />
+    </Routes>
+   
+    </>
+  )
+}
+
+export default App
---
+++ src/demon.jsx
@@ -0,0 +1,11 @@
+     {/* <SidebarItem  text="Dashboard"  active={true} alert /> */}
+      {/* <SidebarItem  text="All Projects" active={true} alert /> */}
+      {/* <SidebarItem  text="Quotation" active={true} alert /> */}
+      {/* <SidebarItem  text="Files" active={true} alert /> */}
+      {/* <SidebarItem Timelines" active={true} alert /> */}
+      {/* <SidebarItem  text="Task Manager" active={true} alert /> */}
+      {/* <SidebarItem  text="MOM" active={true} alert /> */}
+      {/* <SidebarItem  text="Moodboard" active={true} alert /> */}
+      {/* <SidebarItem  text="Vender & PO's" active={true} alert /> */}
+      {/* <SidebarItem  text="Chat" active={true} alert /> */}
+      {/* <SidebarItem text="Reports" active={true} alert /> */}
